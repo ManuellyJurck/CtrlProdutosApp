cca876a6e60c1291bf59edbe5f37b1e5256efe6a
ManuellyJurck <manuellyjurck2019@gmail.com>
2025-10-30 16:36:37 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package br.edu.ifsc.aluno.manuellyjurck.mobile.ctrlprodutos.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
