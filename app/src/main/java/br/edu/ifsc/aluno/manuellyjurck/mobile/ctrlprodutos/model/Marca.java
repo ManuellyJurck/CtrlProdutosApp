5872a7e840582b3def7a7bef5b3e88c347d9c3a3
ManuellyJurck <manuellyjurck2019@gmail.com>
2025-11-13 17:29:15 -0300

    V2.20252213

--- app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/model/Marca.java
+++ app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/model/Marca.java
@@ -5,4 +5,32 @@ public class Marca {
     private String nmMarca;
     private String Qualidade;
 
+    //metodo tostring() para o adapter do spinner
+    @Override
+    public String toString() {
+        return this.nmMarca;
+    }
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    public String getNmMarca() {
+        return nmMarca;
+    }
+
+    public void setNmMarca(String nmMarca) {
+        this.nmMarca = nmMarca;
+    }
+
+    public String getQualidade() {
+        return Qualidade;
+    }
+
+    public void setQualidade(String qualidade) {
+        Qualidade = qualidade;
+    }
 }
--- app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/ui/produto/CadProdutoFragment.java
+++ app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/ui/produto/CadProdutoFragment.java
@@ -7,13 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import br.edu.ifsc.aluno.manuellyjurck.mobile.ctrlprodutos.R;
+import br.edu.ifsc.aluno.manuellyjurck.mobile.ctrlprodutos.model.Marca;
 
 public class CadProdutoFragment extends Fragment {
     private EditText etNome;
@@ -44,8 +56,74 @@ public class CadProdutoFragment extends Fragment {
         //spinner
         this.spMarca = (Spinner) view.findViewById(R.id.spMarca);
         //inicializando a fila de request do SO
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
         this.requestQueue.start();
+        //
+        try {
+            this.consultaMarcas();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
         //return default
         return this.view;
     }
+
+    private void consultaMarcas() throws JSONException {
+        //requisição para o Rest Server
+        JsonArrayRequest jsonArrayReq = null;
+        try {
+            jsonArrayReq = new JsonArrayRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2/consultaMarca.php",
+                    new JSONArray("[{}]"),
+                    response -> {
+                        try {
+                            //se a consulta não veio vazia
+                            if (response != null) {
+                                //array list para receber a resposta
+                                ArrayList<Marca> lista= new ArrayList<>();
+                                //preenchendo ArrayList com JSONArray recebido
+                                for (int pos = 0;pos < response.length();pos++){
+                                    JSONObject jo = response.getJSONObject(pos);
+                                    Marca marca = new Marca();
+                                    marca.setIdMarca(jo.getInt("idMarca"));
+                                    marca.setNmMarca(jo.getString("nmMarca"));
+                                    marca.setQualidade(jo.getString("Qualidade"));
+                                    lista.add(pos, marca);
+                                }
+                                //Criando um adapter para o spinner
+                                ArrayAdapter<Marca> adapter = new ArrayAdapter<>(
+                                        requireContext(),
+                                        android.R.layout.simple_spinner_item,
+                                        lista);
+                               //colocando o adapter no spinner
+                                this.spMarca.setAdapter(adapter);
+                            } else {
+                                Snackbar mensagem = Snackbar.make(view,
+                                        "A consulta não retornou nenhum registro!",
+                                        Snackbar.LENGTH_LONG);
+                                mensagem.show();
+                            }
+                        } catch (JSONException e) {
+                            Snackbar mensagem = Snackbar.make(view,
+                                    "Ops!Problema com o arquivo JSON: " + e,
+                                    Snackbar.LENGTH_LONG);
+                            mensagem.show();
+                        }
+                    },
+                    error -> {
+                        //mostrar mensagem que veio do servidor
+                        Snackbar mensagem = Snackbar.make(view,
+                                "Ops! Houve um problema ao realizar a consulta: " +
+                                        error.toString(), Snackbar.LENGTH_LONG);
+                        mensagem.show();
+                    }
+            );
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        //colocando nova request para fila de execução
+        requestQueue.add(jsonArrayReq);
+    }
+
 }
