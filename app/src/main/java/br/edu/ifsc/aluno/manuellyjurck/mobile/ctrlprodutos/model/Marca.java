6fd2be1b29b2f6be1dc19fb4986dadbd82a60c8f
ManuellyJurck <manuellyjurck2019@gmail.com>
2025-11-13 15:26:53 -0300

    V1.20252213

---
+++ app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/model/Marca.java
@@ -0,0 +1,8 @@
+package br.edu.ifsc.aluno.manuellyjurck.mobile.ctrlprodutos.model;
+
+public class Marca {
+    private int idMarca;
+    private String nmMarca;
+    private String Qualidade;
+
+}
--- app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/ui/produto/CadProdutoFragment.java
+++ app/src/main/java/br/edu/ifsc/aluno/manuellyjurck/mobile/ctrlprodutos/ui/produto/CadProdutoFragment.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.android.volley.RequestQueue;
+
 import br.edu.ifsc.aluno.manuellyjurck.mobile.ctrlprodutos.R;
 
 public class CadProdutoFragment extends Fragment {
@@ -20,6 +22,11 @@ public class CadProdutoFragment extends Fragment {
     private EditText etValor;
     private Spinner spMarca;
     private Button btSalvar;
+
+    private RequestQueue requestQueue;
+    private View view;
+
+
     public CadProdutoFragment() {
         // Required empty public constructor
     }
@@ -33,6 +40,12 @@ public class CadProdutoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cad_produto, container, false);
+        this.view = inflater.inflate(R.layout.fragment_cad_produto, container, false);
+        //spinner
+        this.spMarca = (Spinner) view.findViewById(R.id.spMarca);
+        //inicializando a fila de request do SO
+        this.requestQueue.start();
+        //return default
+        return this.view;
     }
 }
